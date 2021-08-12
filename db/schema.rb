5e7071ebfe6d395fe1d4cbb13cd69cd1d2d8c9a1
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-08-11 22:14:48 -0400

    Add author to posts
    This like magic, to me, I love it:
    
    bin/rails g migration add_author_to_posts author:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_013804) do
+ActiveRecord::Schema.define(version: 2021_08_12_020648) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
