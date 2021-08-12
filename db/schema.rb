1b63a9e7c277edfaf8b4eb0db0406f6e7b293129
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-08-12 08:43:49 -0400

    ch. 3 Validations, Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_020648) do
+ActiveRecord::Schema.define(version: 2021_08_12_122703) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_08_12_020648) do
     t.string "author"
   end
 
+  add_foreign_key "comments", "posts"
 end
